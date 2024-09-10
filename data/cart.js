1c92d203998e73fc5ebdd4a5716df941b6999f54
ReginoOlivencia <tainogaston64@gmail.com>
2024-09-10 13:07:40 -0700

    create automated test for addToCart

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,9 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));
+export let cart;
+
+loadFromStorage();
+
+export function loadFromStorage() {
+  cart = JSON.parse(localStorage.getItem('cart'));
 
 if (!cart) {
   cart = [{
@@ -11,8 +16,7 @@ if (!cart) {
     deliveryOptionId: '2'
   }];
 }
-
-
+}
 
 function saveToStorage() {
   localStorage.setItem('cart', JSON.stringify(cart));
