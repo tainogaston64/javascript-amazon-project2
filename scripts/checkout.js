38180a13d55a05ff0c96deed1157e7fbe4f00bd8
ReginoOlivencia <tainogaston64@gmail.com>
2024-09-20 20:14:55 -0700

    created  class version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,7 +2,7 @@ import {renderCheckoutHeader} from './checkout/checkoutHeader.js';
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 // to import all the data in a file
-import '../data/cart-oop.js';
+import '../data/cart-class.js';
 
 renderCheckoutHeader();
 
