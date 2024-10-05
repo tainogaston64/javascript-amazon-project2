f54bdcc54c2df89ed5a2af5b7115c19ea250fe53
ReginoOlivencia <tainogaston64@gmail.com>
2024-10-05 16:31:53 -0700

    test products for amazon project

--- data/cart-class.js
+++ data/cart-class.js
@@ -1,4 +1,4 @@
-class Cart {
+export class Cart {
   cartItems;
   #localStorageKey;
 
@@ -103,7 +103,7 @@ class Cart {
   }
 }
 
-const cart = new Cart('cart-oop');
+export const cart = new Cart('cart-oop');
 const businessCart = new Cart('cart-business');
 
 console.log(cart);
--- tests/data/productsTest.js
+++ tests/data/productsTest.js
@@ -1,18 +1,51 @@
-import{Product, Clothing, Appliances} from " ../../data/products.js";
+import{Product, Clothing, Appliance} from '../../data/products.js';
 
-describe('test suite: addToCart', () => {
+describe('test suite: Product', () => {
+  let product;
+
   beforeEach(() => {
-    //16e.
-    spyOn(localStorage, 'setItem');
-  })
-  it('adds an existing product to the cart', () => {
-    spyOn(localStorage, 'getItem').and.callFake(() => {
-      return JSON.stringify([{
-        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-        quantity: 1,
-        deliveryOptionId: '1'
-      }]);
+    product =new Product({
+      id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+      name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
     });
-loadFromStorage();
+  });
+
+  it('has the correct properties', () => {
+    expect(product.id).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+  })
+  
 });
-});
+
+describe('test:suite: Clothing', () => {
+  let clothing;
+
+  beforeEach(() => {
+    clothing = new Clothing({
+      id: '',
+      image:'',
+      name: '',
+      sizeChartLink: 'images/clothing-size-chart.png'
+    });
+  });
+  it('has the correct properties', () => {
+    expect(clothing.sizeChartLink).toEqual('images/clothing-size-chart.png')
+  })
+})
+
+describe('test suite: Appliance', () => {
+  let appliance;
+
+  beforeEach(() => {
+    appliance = new Appliance({
+      id: '',
+      image:'',
+      name: '',
+      instructionsLink: '',
+      warrantyLink: 'images/appliance-warranty.png'
+    });
+  });
+  it('has the correct properties', () => {
+    expect(appliance.warrantyLink).toEqual('images/appliance-warranty.png')
+  })
+})
