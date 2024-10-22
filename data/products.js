6a3574acc3d08f1105146b678e44b14f1cf6d74b
ReginoOlivencia <tainogaston64@gmail.com>
2024-10-22 05:36:34 -0700

    added error handling

--- data/products.js
+++ data/products.js
@@ -168,10 +168,13 @@ export function loadProductsFetch() {
     });
 
     console.log('load products');
+  }).catch((error) => {
+    console.log('Unexpected error . Please try again later');
   });
 
   return promise;
 }
+
 // loadProductsFetch().then(() => {
 //   console.log('next step');
 // });
@@ -194,12 +197,13 @@ xhr.addEventListener('load', () => {
     fun();
 });
 
+xhr.addEventListener('error', (error) => {
+  console.log('Unexpected error . Please try again later');
+});
+
   xhr.open('GET', 'https://supersimplebackend.dev/products');
   xhr.send();
 }
-
-
-
 
 // export const products = [
 //   {
